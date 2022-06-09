a1763ef57d24c4500b716d6283296c93c85b3a60
Vakurin <vakurin.maxim@gmail.com>
2022-06-09 21:06:18 +0300

    gitignore and eslintrc file

---
+++ .eslintrc.js
@@ -0,0 +1,65 @@
+module.exports = {
+    env: {
+      browser: true,
+      es6: true,
+    },
+    extends: [
+      'plugin:react/recommended',
+    ],
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 2018,
+      sourceType: 'module',
+    },
+    plugins: [
+      'react',
+    ],
+    rules: { 
+      "jsx-a11y/anchor-is-valid": "off",
+      "jsx-a11y/anchor-has-content": "off",
+
+      "import/prefer-default-export": 0,
+      "max-len": [
+        2,
+        250
+      ],
+      "no-multiple-empty-lines": [
+        "error",
+        {
+          "max": 1,
+          "maxEOF": 1
+        }
+      ],
+      "no-underscore-dangle": [
+        "error",
+        {
+          "allow": [
+            "_d",
+            "_dh",
+            "_h",
+            "_id",
+            "_m",
+            "_n",
+            "_t",
+            "_text"
+          ]
+        }
+      ],
+      "object-curly-newline": 0,
+      "react/prop-types": 0,
+      "react/jsx-filename-extension": 0,
+      "react/jsx-one-expression-per-line": 0,
+      "jsx-a11y/click-events-have-key-events": 0,
+      "jsx-a11y/alt-text": 0,
+      "jsx-a11y/no-autofocus": 0,
+      "jsx-a11y/no-static-element-interactions": 0,
+      "react/no-array-index-key": 0,
+    }
+  };
+  
