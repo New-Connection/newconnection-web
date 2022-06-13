ceb946adb30a542e269d18a1b3b05c82938bf7b5
Vakurin <vakurin.maxim@gmail.com>
2022-06-13 19:20:27 +0300

    Add connect wallet

--- src/componets/Header/Header.js
+++ src/componets/Header/Header.js
@@ -1,47 +1,56 @@
-import { React } from 'react';
+import React, { useState } from 'react';
 import { ethers } from "ethers";
 import './Header.css';
 import logo from '../../assets/logo.png';
-import twitter_img from '../../assets/twitter.png'
-import discord_img from '../../assets/discord.png'
 
-const openInNewTab = url => {
-    window.open(url, '_blank', 'noopener,noreferrer');
-};
-const TwitterURL = 'https://twitter.com/NewConnectionX'
+const Header = () => {
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [defaultAccount, setDefaultAccount] = useState(null);
+    const [connectButtonText, setConnectButtonText] = useState('Connect Wallet');
 
-function SocialMedia() {
-    return (
-        <div className='btns'>
-            <button onClick={()=> connectWallet()} className="social-btn">Connect Wallet</button>
-        </div>
-    )
-}
+    // update account, will cause component re-render
+    const accountChangedHandler = (newAccount) => {
+        setDefaultAccount(newAccount);
+    }
 
-function Logo(){
-    return(
-        <div className='logo'>
-            <img src={logo}></img>
-            <p className='logo-spacing'>new<br />connection</p>
-        </div>
-        
-    )
-}
-
-async function connectWallet(){
-    const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
-    // Prompt user for account connections
-    await provider.send("eth_requestAccounts", []);
-    const signer = provider.getSigner();
-    console.log("Account:", await signer.getAddress());
-}
-
-function Header() {
+    const connectWalletHandler = () => {
+		if (window.ethereum && window.ethereum.isMetaMask) {
+			const accounts = window.ethereum.request({ method: 'eth_requestAccounts'})
+            accounts.then(result => {
+                accountChangedHandler(result[0]);
+                setConnectButtonText('Account:');
+            }).catch(error => {
+                setErrorMessage(error.message)
+            })
+        } else {
+			console.log('Need to install MetaMask');
+			setErrorMessage('Please install MetaMask browser extension to interact');
+		}
+    }
+			
+    function ConnectWallet() {
+        return (
+            <div className='btns'>
+                <button onClick={()=> connectWalletHandler()} className="wallet-button">{connectButtonText} {defaultAccount}</button>
+                <p>{errorMessage}</p>
+            </div>
+        )
+    }
+    
+    function Logo(){
+        return(
+            <div className='logo'>
+                <img src={logo}></img>
+                <p className='logo-spacing'>new<br />connection</p>
+            </div>
+            
+        )
+    }
     return (
         <header>
             <nav className='header'>
                 <Logo />
-                <SocialMedia />
+                <ConnectWallet />
             </nav>
         </header>
     )
