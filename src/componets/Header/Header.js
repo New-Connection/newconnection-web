964dcb85bf17c7b692b91b7384e55e00e404d17c
Vakurin <vakurin.maxim@gmail.com>
2022-06-14 15:29:17 +0300

    Set Account Wallet

--- src/componets/Header/Header.js
+++ src/componets/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ethers } from "ethers";
 import './Header.css';
 import logo from '../../assets/logo.png';
@@ -13,12 +13,23 @@ const Header = () => {
         setDefaultAccount(newAccount);
     }
 
+    const chainChangedHandler = () => {
+		// reload the page to avoid any errors with chain change mid use of application
+		window.location.reload();
+	}
+
+	// listen for account changes
+	//window.ethereum.on('accountsChanged', accountChangedHandler);
+    
+    // Event for chain changes
+	window.ethereum.on('chainChanged', chainChangedHandler);
+
     const connectWalletHandler = () => {
 		if (window.ethereum && window.ethereum.isMetaMask) {
 			const accounts = window.ethereum.request({ method: 'eth_requestAccounts'})
             accounts.then(result => {
                 accountChangedHandler(result[0]);
-                setConnectButtonText('Account:');
+                setConnectButtonText('Account:')
             }).catch(error => {
                 setErrorMessage(error.message)
             })
@@ -29,10 +40,9 @@ const Header = () => {
     }
 			
     function ConnectWallet() {
-        return (
+        return(
             <div className='btns'>
                 <button onClick={()=> connectWalletHandler()} className="wallet-button">{connectButtonText} {defaultAccount}</button>
-                <p>{errorMessage}</p>
             </div>
         )
     }
