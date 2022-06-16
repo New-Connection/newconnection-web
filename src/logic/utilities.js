0fc446a7bb56ea38e5ca1714093520393673cefe
Vakurin <vakurin.maxim@gmail.com>
2022-06-16 16:48:40 +0300

    Refactoring

--- src/logic/utilities.js
+++
@@ -1,9 +0,0 @@
-import fetch from "node-fetch";
-
-export default async function read_file(filename){
-    await fetch(filename)
-    .then(response => response.text())
-    .then(text => {
-        return text
-    })
-}
