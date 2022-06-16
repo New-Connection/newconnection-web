fa8bf47629f71716db2bb515685e635ac465e0eb
Vakurin <vakurin.maxim@gmail.com>
2022-06-16 16:49:02 +0300

    change len files

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,7 +27,7 @@ module.exports = {
       "import/prefer-default-export": 0,
       "max-len": [
         2,
-        7000
+        256
       ],
       "no-multiple-empty-lines": [
         "error",
