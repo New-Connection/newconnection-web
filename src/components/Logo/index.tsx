544d8828a0f5d73dd35fe9ef4d2631d0cba4df42
Vakurin <vakurin.maxim@gmail.com>
2022-06-20 00:10:04 +0300

    logo updated

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -5,7 +5,7 @@ const logo =  require("../../assets/logo.png")
 const Logo = () => {
     return(
         <div className="flex">
-            <img className='h-12 w-5 pr-2' src={logo} alt='logo'/>
+            <img className='h-11 w-5 pr-2' src={logo} alt='logo'/>
             <p>new<br />connection</p>
         </div>
     )
