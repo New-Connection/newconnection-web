7db83b8a0e75bc0bb6759af64bbb7031ccab3aba
Vakurin <vakurin.maxim@gmail.com>
2022-06-19 20:34:44 +0300

    added new plugins

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+const plugin = require('tailwindcss/plugin');
+
+module.exports = {
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    plugin(({ addVariant }) => {
+      addVariant('enter', '&[data-enter]');
+      addVariant('leave', '&[data-leave]');
+      addVariant('active-item', '&[data-active-item]');
+
+      addVariant('active', ['&:active', '&[data-active]']);
+      addVariant('focus-visible', ['&:focus-visible', '&[data-focus-visible]']);
+      addVariant('aria-invalid', '&[aria-invalid="true"]');
+      addVariant('aria-disabled', '&[aria-disabled="true"]');
+      addVariant('aria-selected', '&[aria-selected="true"]');
+      addVariant('aria-expanded', '&[aria-expanded="true"]');
+      addVariant('aria-checked', '&[aria-checked="true"]');
+    }),
+  ],
+};
