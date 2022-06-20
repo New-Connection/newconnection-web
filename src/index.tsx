48246e78ffc3458913a53bf9009ac8620820e741
Vakurin <vakurin.maxim@gmail.com>
2022-06-20 03:01:32 +0300

    add query and wagmi configs

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from 'next-themes';
-
+import { QueryClient, QueryClientProvider} from 'react-query'; // it makes fetching, caching, synchronizing and updating server state
+import { WagmiConfig } from 'wagmi'
 // code
 import Home from './pages';
 import './styles/globals.css';
+import  { walletConfig } from './components/Web3/'
 
 function Page() {
+    const [queryClient] = React.useState(() => new QueryClient());
     return (
         <ThemeProvider defaultTheme="system" attribute="class">
-            <Home />
+            <WagmiConfig client={walletConfig}>
+                <QueryClientProvider client={queryClient}>
+                    <Home />
+                </QueryClientProvider>
+            </WagmiConfig>
         </ThemeProvider>
     )
 }
