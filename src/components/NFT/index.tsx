ca1108acd5af9c6097e560648cffea6b98432bea
Vakurin <vakurin.maxim@gmail.com>
2022-06-22 19:09:32 +0300

    add new input element

--- src/components/NFT/index.tsx
+++ src/components/NFT/index.tsx
@@ -2,51 +2,24 @@ import * as React from 'react';
 import { InputAmount, InputText, SubmitButton } from '../Form';
 
 export default function NFTSection(){
+
+
+
     return (
       <section className="relative w-full">
       <form className="mx-auto flex max-w-xl flex-col gap-4">
         <h1 className="font-exo my-2 text-2xl font-semibold text-[#3D3D3D] dark:text-white">Create NFT</h1>
-        <InputText label={'Name'} name="recipientAddress" isRequired />
-        <InputText label={'Description'} name="vestedToken" isRequired />
-        <InputAmount label={'Number of NFT'} name="vestingAmount"  isRequired/>
-        {/* <InputAmountWithDuration
-          label={'Vesting Duration'}
-          name="vestingTime"
-          isRequired
-          selectInputName="vestingDuration"
-          handleChange={(e) => handleChange(e.target.value, 'vestingTime')}
-          handleSelectChange={(e) => handleChange(e.target.value, 'vestingDuration')}
-        />
-        {formData.includeCliff && (
-          <InputAmountWithDuration
-            label={'Cliff Duration'}
-            name="cliffTime"
-            isRequired
-            selectInputName="cliffDuration"
-            handleChange={(e) => handleChange(e.target.value, 'cliffTime')}
-            handleSelectChange={(e) => handleChange(e.target.value, 'cliffDuration')}
-          />
-        )}
-        {formData.includeCustomStart && (
-          <InputText
-            label={'Start Date (YYYY-MM-DD)'}
-            name="startDate"
-            isRequired
-            placeholder="YYYY-MM-DD"
-            pattern="\d{4}-\d{2}-\d{2}"
-            handleChange={(e) => handleChange(e.target.value, 'startDate')}
-          />
-        )} */}
-
-
+        <InputText label={'Name'} name="recipientAddress" placeholder='NFT Name' isRequired />
+        <InputText label={'Description'} name="vestedToken" placeholder='A short descption about NFT collection(Max. 250 words)' isRequired />
+        <div className="flex space-x-4">
+            <InputAmount label={'Number of NFT'} name="vestingAmount" isRequired/>
+            <InputAmount label={'Price'} placeholder="0 (Max. 0)" name="vestingAmount" isRequired/>
+        </div>
+        <div className="flex space-x-4">
+            <InputText label={'Twitter (optional)'} name="twitter" placeholder='Enter your twitter handler' isRequired={false}/>
+            <InputText label={'Discord (optional)'} name="discord" placeholder='Enter your discord server' isRequired={false}/>
+        </div>
         <SubmitButton className="mt-5">
-          {/* {checkingApproval || approvingToken ? (
-            <BeatLoader size={6} color="white" />
-          ) : isApproved ? (
-            'Create Contract'
-          ) : (
-            'Approve Token'
-          )} */}
           Create Contract
         </SubmitButton>
       </form>
