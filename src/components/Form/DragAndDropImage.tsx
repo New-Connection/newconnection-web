65373cce8ea1dea2a26f7dd21c40d1203c59dd0e
Vakurin <vakurin.maxim@gmail.com>
2022-06-23 20:39:48 +0300

    [FIX] error with type

--- src/components/Form/DragAndDropImage.tsx
+++ src/components/Form/DragAndDropImage.tsx
@@ -5,7 +5,7 @@ import ImageIcon from "../../assets/ImageIcon.png"
 const fileTypes = ["JPEG", "PNG", "GIF"];
 
 export function DragAndDropImage() {
-  const [file, setFile] = useState(null);
+  const [file, setFile] = useState<any | null>(null);
   const handleChange = (file) => {
     setFile(file);
   };
@@ -21,8 +21,8 @@ export function DragAndDropImage() {
         types={fileTypes}
       >
         <div className="flex flex-row gap-x-3 p-14 border-dashed 
-        rounded-md border-2 border-slate-800
-        bg-slate-800 hover:border-slate-300">
+          rounded-md border-2 border-slate-800
+          bg-slate-800 hover:border-slate-300">
           <img src={ImageIcon.src} className='h-6 w-6'/>
           <p className='text-slate-400'>PNG, JPEG and JPG accept. Max 1mb.</p>
         </div>
