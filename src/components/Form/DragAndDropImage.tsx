119f790c7e7e0f6a656061e6a85d098b7beea753
Vakurin <vakurin.maxim@gmail.com>
2022-06-23 18:38:53 +0300

    Drag and Drop implementation

---
+++ src/components/Form/DragAndDropImage.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { FileUploader } from "react-drag-drop-files";
+import ImageIcon from "../../assets/ImageIcon.png"
+
+const fileTypes = ["JPEG", "PNG", "GIF"];
+
+export function DragAndDropImage() {
+  const [file, setFile] = useState(null);
+  const handleChange = (file) => {
+    setFile(file);
+  };
+  return (
+    <>
+      <span className="input-label">File</span>
+      <FileUploader
+        hoverTitle='Drop here'
+        multiple={false}
+        handleChange={handleChange}
+        label='Max 3 MB.'
+        name="file"
+        types={fileTypes}
+      >
+        <div className="flex flex-row gap-x-3 p-14 border-dashed 
+        rounded-md border-2 border-slate-800
+        bg-slate-800 hover:border-slate-300">
+          <img src={ImageIcon.src} className='h-6 w-6'/>
+          <p className='text-slate-400'>PNG, JPEG and JPG accept. Max 1mb.</p>
+        </div>
+      </FileUploader>    
+      <p>{file ? `File name: ${file?.name}` : "no files uploaded yet"}</p>
+    </>
+  );
+}
--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -2,3 +2,4 @@ export { InputAmount, InputText } from './Input';
 export { SubmitButton } from './Button';
 export { BlockchainSelector } from './BlockchainSelector';
 export { TypeSelector } from './TypeSelector';
+export { DragAndDropImage } from './DragAndDropImage';
