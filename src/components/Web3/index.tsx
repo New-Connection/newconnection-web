1da9902ac9252118f453197ce60d9e356816d661
Vakurin <vakurin.maxim@gmail.com>
2022-06-25 02:53:42 +0300

    provider added

--- src/components/Web3/index.tsx
+++ src/components/Web3/index.tsx
@@ -1,4 +1,4 @@
 export { WalletSelector } from './WalletSelector';
-export { walletConfig } from './walletConfig';
+export { walletConfig, provider } from './walletConfig';
 export { WalletProfile } from './WalletProfile';
 export { Account } from './Account';
