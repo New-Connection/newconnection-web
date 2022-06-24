94a379a7fec0f7cb540bda46d2b7e1e62131c73e
hokage229 <ernestzulpukarov@gmail.com>
2022-06-24 15:46:18 +0600

    add Head to pages

--- src/pages/create-nft.tsx
+++ src/pages/create-nft.tsx
@@ -1,14 +1,20 @@
-import Layout from '../components/Layout';
-import * as React from 'react';
-import { NextPage } from 'next'; // it's just type 
-import NFTSection from '../components/NFT';
+import Layout from "../components/Layout";
+import * as React from "react";
+import { NextPage } from "next"; // it's just type
+import NFTSection from "../components/NFT";
+import Head from "next/head";
 
 const CreateNFT: NextPage = () => {
-  return (
-    <Layout className="app-section mx-auto mt-12 flex w-full flex-col items-center space-y-6 pb-8 dark:bg-[#161818]">
-        <h1>Create NFT</h1>
-    </Layout>
-  );
+    return (
+        <>
+            <Head>
+                <title>Create NFT</title>
+            </Head>
+            <Layout className="app-section mx-auto mt-12 flex w-full flex-col items-center space-y-6 pb-8 dark:bg-[#161818]">
+                <h1>Create NFT</h1>
+            </Layout>
+        </>
+    );
 };
 
 export default CreateNFT;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,19 @@
-import * as React from 'react';
+import * as React from "react";
 
-import Layout from '../components/Layout';
+import Layout from "../components/Layout";
+import Head from "next/head";
 
 const Home = () => {
-  return (
-    <Layout className="dark: flex flex-col gap-[30px] dark:bg-[#161818]" noBanner={false}>
-    </Layout>
-  );
+    return (
+        <>
+            <Head>
+                <title>New Connection</title>
+            </Head>
+            <Layout className="dark: flex flex-col gap-[30px] dark:bg-[#161818]" noBanner={false}>
+                <div>Home</div>
+            </Layout>
+        </>
+    );
 };
 
 export default Home;
