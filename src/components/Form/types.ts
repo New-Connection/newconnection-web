f15eef0d24fa8c693a6bbd64d78a7d5051e411b1
hokage229 <ernestzulpukarov@gmail.com>
2022-06-28 21:01:06 +0600

    refactor InputText component

--- src/components/Form/types.ts
+++ src/components/Form/types.ts
@@ -1,51 +1,51 @@
-import { VoidSigner } from 'ethers';
-import * as React from 'react';
+import { VoidSigner } from "ethers";
+import * as React from "react";
 
 export interface InputElement {
-  name: string;
-  label: string;
-  isRequired: boolean;
-  className?: string;
-  handleChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  placeholder?: string;
-  optional?: boolean;
-  pattern?: string;
+    name: string;
+    label: string;
+    isRequired: boolean;
+    className?: string;
+    handleChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    placeholder?: string;
+    optional?: boolean;
+    maxLength?: number;
+    pattern?: string;
 }
 
 export interface Selector {
-  name: string;
-  label: string;
-  className?: string;
-  handlerChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    name: string;
+    label: string;
+    className?: string;
+    handlerChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export interface IDragAndDrop{
-  name: string;
-  label: string;
-  className?: string;
-  handlerChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  hoverTitle?: string;
-  multiplefiles?:boolean;
+export interface IDragAndDrop {
+    name: string;
+    label: string;
+    className?: string;
+    handlerChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    hoverTitle?: string;
+    multipleFiles?: boolean;
 }
-
 
 export interface InputWithTokenElement extends InputElement {
-  handleTokenChange: (token: string) => void;
-  tokens: string[];
+    handleTokenChange: (token: string) => void;
+    tokens: string[];
 }
 
 export interface InputWithTokenSelectProps extends InputElement {
-  handleTokenChange: (token: string) => void;
-  handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  tokenOptions: string[];
+    handleTokenChange: (token: string) => void;
+    handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    tokenOptions: string[];
 }
 
 export interface InputAmountWithDaysProps {
-  name: string;
-  selectInputName: string;
-  label: string;
-  isRequired: boolean;
-  className?: string;
-  handleChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  handleSelectChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+    name: string;
+    selectInputName: string;
+    label: string;
+    isRequired: boolean;
+    className?: string;
+    handleChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    handleSelectChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
 }
