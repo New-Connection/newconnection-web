ef19fad7531325c2f7b5cd716afe83207ba08514
hokage229 <ernestzulpukarov@gmail.com>
2022-06-28 21:00:10 +0600

    refactor DragAndDropImage component

--- src/components/Form/DragAndDropImage.tsx
+++ src/components/Form/DragAndDropImage.tsx
@@ -1,33 +1,41 @@
-import { useState } from "react";
 import { FileUploader } from "react-drag-drop-files";
-import ImageIcon from "../../assets/ImageIcon.png"
+import ImageIcon from "../../assets/ImageIcon.png";
+import Image from "next/image";
 
-import classNames from 'classnames';
-import { IDragAndDrop } from './types';
+import { IDragAndDrop } from "./types";
 
-export const DragAndDropImage = ({label, name, className, multiplefiles=false, hoverTitle, handlerChange, ...props}:IDragAndDrop) => {
-  
-  const fileTypes = ["JPEG", "PNG", "JPG"];
-  
-  return (
-    <>
-      <span className="input-label">{label}</span>
-      <FileUploader
-        hoverTitle={hoverTitle}
-        multiple={multiplefiles}
-        handleChange={handlerChange}
-        name={name}
-        types={fileTypes}
-      >
-        <div className="flex flex-row gap-x-3 py-14 px-3 border-dashed 
-          rounded-md border-2 border-slate-300
-          bg-slate-800 bg-[#fdfdfda6] hover:border-slate-800 items-center">
-          <p className='text-slate-500'>PNG, JPEG and JPG accept. Max 1mb.</p>
-          {/* <img src={ImageIcon.src} className='h-6 w-6'/> */}
-          {/* <p className='text-slate-400'>{file ? `File name: ${file?.name} ✅` : "PNG, JPEG and JPG accept. Max 1mb."}</p> */}
+const fileTypes = ["JPEG", "PNG", "JPG"];
+
+export const DragAndDropImage = ({
+    label,
+    name,
+    className,
+    multipleFiles = false,
+    hoverTitle,
+    handlerChange,
+    ...props
+}: IDragAndDrop) => {
+    return (
+        <div className={className}>
+            <span className="input-label">{label}</span>
+            <FileUploader
+                hoverTitle={hoverTitle}
+                multiple={multipleFiles}
+                handleChange={handlerChange}
+                name={name}
+                types={fileTypes}
+                {...props}
+            >
+                <div
+                    className="flex flex-col text-center py-6 px-3 mt-[5px] border-dashed
+                               rounded-md border-2 border-slate-300
+                               bg-slate-800 bg-[#fdfdfda6] hover:border-slate-800 items-center h-[200px]"
+                >
+                    <Image src={ImageIcon} width={"50"} height={"50"} />
+                    <p className="text-slate-500 mt-5">PNG, JPEG and JPG accept. Max 1mb.</p>
+                    {/* <p className='text-slate-400'>{file ? `File name: ${file?.name} ✅` : "PNG, JPEG and JPG accept. Max 1mb."}</p> */}
+                </div>
+            </FileUploader>
         </div>
-      </FileUploader>    
-      
-    </>
-  );
-}
+    );
+};
