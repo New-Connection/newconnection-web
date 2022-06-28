351fdd786c5af3e7d6141515aae70bdc8a6cf20a
Vakurin <vakurin.maxim@gmail.com>
2022-06-29 01:39:46 +0300

    delete p from div tag

--- src/components/Web3/WalletSelector.tsx
+++ src/components/Web3/WalletSelector.tsx
@@ -51,7 +51,7 @@ export const WalletSelector = ({ dialog }: Props) => {
                             Connected to {connector?.name}
                         </p>
                         <p className="flex items-center gap-4 break-words text-slate-500">
-                            <div>{ensName ? `${ensName} (${formattedAddress})` : address}</div>
+                            {ensName ? `${ensName} (${formattedAddress})` : address}
                         </p>
                         <button className="nav-button mt-5" onClick={() => handlerDisconect()}>
                             Disconnect
