ea6952b72dbe2a5a330a1f1192bd57477a632e57
hokage229 <ernestzulpukarov@gmail.com>
2022-06-29 14:30:37 +0600

    fix changeEventHandler for input and textarea; refactor form types

--- src/components/Form/Button.tsx
+++ src/components/Form/Button.tsx
@@ -1,18 +1,22 @@
-import * as React from 'react';
-import classNames from 'classnames';
+import * as React from "react";
+import classNames from "classnames";
+import { ButtonProps } from "./types";
 
-interface ButtonProps {
-  disabled?: boolean;
-  type?: 'submit' | 'button';
-  className?: string;
-  onClick?: () => void;
-  children: React.ReactNode;
-}
-
-export const SubmitButton = ({ disabled = false, type = 'submit', className, children, ...props }: ButtonProps) => {
-  return (
-    <button className={classNames('form-submit-button', className)} type={type} disabled={disabled} {...props}>
-      {children}
-    </button>
-  );
+export const SubmitButton = ({
+    disabled = false,
+    type = "submit",
+    className,
+    children,
+    ...props
+}: ButtonProps) => {
+    return (
+        <button
+            className={classNames("form-submit-button", className)}
+            type={type}
+            disabled={disabled}
+            {...props}
+        >
+            {children}
+        </button>
+    );
 };
