90cd2bd43b5654d30649be7e590e81776ed6ec06
Vakurin <vakurin.maxim@gmail.com>
2022-06-29 14:54:45 +0300

    clean up with ABIs

--- src/utils/contract.ts
+++ src/utils/contract.ts
@@ -1,10 +1,10 @@
-import governanceNFT from "abis/GovernanceNFT";
+import { GovernorNFTABI } from "abis/GovernanceNFT";
 import governor from "abis/Governor";
 
 import { ContractFactory, Signer } from "ethers";
 
 export const createFactoryNFT = (bitecode: any, signer: Signer) =>
-    new ContractFactory(governanceNFT, bitecode, signer);
+    new ContractFactory(GovernorNFTABI, bitecode, signer);
 
 export const createFactoryGovernor = (bitecode: any, signer: Signer) =>
     new ContractFactory(governor, bitecode, signer);
