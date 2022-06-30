7c78ae8f575e9538cc7d32d15249b4ba6d5cbed7
hokage229 <ernestzulpukarov@gmail.com>
2022-06-30 19:06:37 +0600

    Add Checkbox component, Checkbox styles

---
+++ src/components/Form/Checkbox.tsx
@@ -0,0 +1,29 @@
+import { Checkbox, useCheckboxState } from "ariakit/checkbox";
+import { Group, GroupLabel } from "ariakit/group";
+
+import styles from "styles/components/Form/Checkmox.module.css";
+import { CheckboxProps } from "./types";
+import { ChangeEvent, ChangeEventHandler } from "react";
+
+export function CheckboxGroup({ label, description, values, handleChange }: CheckboxProps) {
+    return (
+        <Group>
+            <GroupLabel className={"input-label"}>{label}</GroupLabel>
+            <div className={"input-label text-xs text-gray-500 mb-2"}>{description}</div>
+            <div className="flex flex-row gap-6 flex-wrap">
+                {values.map((value) => (
+                    <label key={value.toUpperCase()} className="input-label">
+                        <Checkbox
+                            as="button"
+                            value={value}
+                            className={styles.checkbox}
+                            onChange={handleChange}
+                        >
+                            {value}
+                        </Checkbox>
+                    </label>
+                ))}
+            </div>
+        </Group>
+    );
+}
