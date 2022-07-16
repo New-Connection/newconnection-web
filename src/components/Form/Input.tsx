37d0e0ca0c22e0c471dbbebe45d7f184aa8e69e6
Vakurin <vakurin.maxim@gmail.com>
2022-07-16 21:19:34 +0200

    added disable argument and input supply of nft

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,4 +1,5 @@
 import { InputAmountProps, InputTextProps } from "./types";
+import Image, { StaticImageData } from "next/image";
 
 export const InputAmount = ({
     name,
@@ -10,6 +11,7 @@ export const InputAmount = ({
     min,
     max,
     step,
+    isDisabled,
     ...props
 }: InputAmountProps) => {
     return (
@@ -37,6 +39,7 @@ export const InputAmount = ({
                 max={max || 100} //Max number of NFT
                 step={step || 1}
                 onChange={handleChange}
+                disabled={isDisabled}
                 {...props}
             />
         </div>
@@ -108,6 +111,56 @@ export const InputTextArea = ({
                 maxLength={maxLength}
                 {...props}
             />
+        </div>
+    );
+};
+
+export const InputSupplyOfNFT = ({
+    name,
+    label,
+    labelTitle,
+    isRequired,
+    className,
+    placeholder,
+    handleChange,
+    image,
+    min,
+    max,
+    step,
+    ...props
+}: InputAmountProps) => {
+    return (
+        <div className={className}>
+            <label>
+                <div title={labelTitle} className="sub-label">
+                    {label}
+                </div>
+            </label>
+            <div className="relative">
+                <input
+                    className={"input-field"}
+                    name={name}
+                    required={isRequired}
+                    autoComplete="off"
+                    autoCorrect="off"
+                    type="number"
+                    pattern="^[0-9]*[.,]?[0-9]*$"
+                    placeholder="1"
+                    minLength={1}
+                    maxLength={79}
+                    spellCheck="false"
+                    inputMode="decimal"
+                    title="Enter numbers only."
+                    min={min || 1}
+                    max={max || 100} //Max number of NFT
+                    step={step || 1}
+                    onChange={handleChange}
+                    {...props}
+                />
+                <div className="flex absolute inset-y-0 right-0 items-center pr-2  pointer-events-none">
+                    <Image src={image as StaticImageData} height="25" width="25" />
+                </div>
+            </div>
         </div>
     );
 };
