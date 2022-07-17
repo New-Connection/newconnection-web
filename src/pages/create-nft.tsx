f93f3d5667eda811a2c36caa7c2cb6fe8ba07b19
Vakurin <vakurin.maxim@gmail.com>
2022-07-17 22:30:39 +0200

    added blockchain images

--- src/pages/create-nft.tsx
+++ src/pages/create-nft.tsx
@@ -40,14 +40,20 @@ import Ethereum from "assets/chains/Ethereum.png";
 import Polygon from "assets/chains/Polygon.png";
 import Arbitrum from "assets/chains/Arbitrum.png";
 import Binance from "assets/chains/Binance.png";
+import Avalanche from "assets/chains/Avalanche.png";
+import Fantom from "assets/chains/Fantom.png";
+import Optimism from "assets/chains/Optimism.png";
 
-const chains = ["Ethereum", "Polygon", "Arbitrum", "Binance"];
+const chains = ["Ethereum", "Polygon", "Arbitrum", "Binance", "Avalanche", "Fantom", "Optimism"];
 
 const images = {
     Ethereum: Ethereum,
     Polygon: Polygon,
     Arbitrum: Arbitrum,
     Binance: Binance,
+    Avalanche: Avalanche,
+    Fantom: Fantom,
+    Optimism: Optimism,
 };
 
 const CreateNFT: NextPage = () => {
