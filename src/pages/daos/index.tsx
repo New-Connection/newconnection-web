66d5663a655e4e5d4e8501a1d677f6df2108b09a
hokage229 <ernestzulpukarov@gmail.com>
2022-07-20 20:10:33 +0600

    refactor daos page

--- src/pages/daos/index.tsx
+++ src/pages/daos/index.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
-import type { GetStaticProps, NextPage } from "next";
+import type { NextPage } from "next";
 import Layout from "components/Layout/Layout";
 import Head from "next/head";
 import Link from "next/link";
 import { useMoralisQuery } from "react-moralis";
 import { useEffect, useState } from "react";
 import Moralis from "moralis";
+import Image from "next/image";
+import basicAvatar from "assets/basic_avatar.jpg";
 
 const DAOsPage: NextPage = () => {
     const [DAOs, setDAOs] = useState<Moralis.Object<Moralis.Attributes>[]>();
@@ -29,23 +31,90 @@ const DAOsPage: NextPage = () => {
         fetchDB();
     }, []);
 
+    const DAOCard = ({ name, description, profileImage, address, isActive, proposals, votes }) => {
+        return (
+            <Link href={`/daos/${address}`}>
+                <div
+                    className={
+                        // border-[#6858CB]
+                        "flex justify-between w-full h-36 p-3 mt-3 rounded border-b-2 cursor-pointer"
+                    }
+                >
+                    <div className={"flex gap-3 w-2/3 overflow-hidden"}>
+                        <div className={"w-28 h-28"}>
+                            <Image className={"w-28 h-28"} src={basicAvatar} />
+                        </div>
+                        <div className="w-2/3">
+                            <p className={"text-lg uppercase font-semibold"}>{name}</p>
+                            <p className={"text-gray-500 "}>{description}</p>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col w-32 text-center text-xs">
+                        {isActive ? (
+                            <p
+                                className={
+                                    "font-semibold border-gray-200 bg-gray-200 mb-3 px-1 border-2 rounded-3xl"
+                                }
+                            >
+                                Active voting now
+                            </p>
+                        ) : (
+                            <p className={""}>No active voting</p>
+                        )}
+                        <div className={"flex flex-col gap-3"}>
+                            <div className={"flex justify-between"}>
+                                <div className={"text-gray-500"}>Proposals:</div>
+                                <div>{proposals}</div>
+                            </div>
+                            <div className={"flex justify-between"}>
+                                <div className={"text-gray-500"}>Votes:</div>
+                                <div>{votes}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Link>
+        );
+    };
+
     return (
         <div>
             <Head>
                 <title>DAOs</title>
             </Head>
-            <Layout className="app-section mx-auto mt-12 flex w-full flex-col items-center space-y-6 pb-8 bg-[#ffffff]">
+            <Layout className="app-section mx-auto mt-32 flex w-full flex-col items-center space-y-6 pb-8 bg-[#ffffff]">
                 <section className="app-section flex h-full flex-1 flex-col gap-[50px]">
-                    <h1>DAOs</h1>
+                    <div className={"flex justify-between items-center"}>
+                        <h1 className={"text-highlighter"}>DAOs</h1>
+                        <Link href="./create-dao">
+                            <button className={"secondary-button h-10 "}>Create DAO</button>
+                        </Link>
+                    </div>
+
                     <ul>
                         {DAOs &&
                             DAOs.map((dao) => {
                                 const address = dao.get("contractAddress");
-                                const chainId = dao.get("chainId");
+                                const name = dao.get("name");
+                                const description = dao.get("description");
+                                const profileImage = dao.get("profileImage");
+
+                                //todo: write to db
+                                const isActive = true;
+                                const proposals = 0;
+                                const votes = 0;
                                 return (
                                     <li key={address}>
-                                        <Link href={`/daos/${address}`}>{address}</Link> (Network:
-                                        {chainId})
+                                        <DAOCard
+                                            name={name}
+                                            description={description}
+                                            address={address}
+                                            profileImage={profileImage}
+                                            isActive={isActive}
+                                            proposals={proposals}
+                                            votes={votes}
+                                        />
                                     </li>
                                 );
                             })}
