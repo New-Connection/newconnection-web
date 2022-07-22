8ddacb151366f5ad4d9dcaa337985b3ff443fe30
Vakurin <vakurin.maxim@gmail.com>
2022-07-22 20:37:58 +0200

    added blockchain file

---
+++ src/utils/blockchains.ts
@@ -0,0 +1,19 @@
+import Ethereum from "assets/chains/Ethereum.png";
+import Polygon from "assets/chains/Polygon.png";
+import Arbitrum from "assets/chains/Arbitrum.png";
+import Binance from "assets/chains/Binance.png";
+import Avalanche from "assets/chains/Avalanche.png";
+import Fantom from "assets/chains/Fantom.png";
+import Optimism from "assets/chains/Optimism.png";
+
+export const CHAINS = ["Ethereum", "Polygon", "Arbitrum", "Binance", "Avalanche", "Fantom", "Optimism"];
+
+export const CHAINS_IMG = {
+    Ethereum: Ethereum,
+    Polygon: Polygon,
+    Arbitrum: Arbitrum,
+    Binance: Binance,
+    Avalanche: Avalanche,
+    Fantom: Fantom,
+    Optimism: Optimism,
+};
