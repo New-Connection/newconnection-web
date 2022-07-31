8b533eea5aff0c11ae5e84691022173945beaa39
Vakurin <vakurin.maxim@gmail.com>
2022-07-31 19:33:46 +0300

    update send button

--- src/pages/daos/add-new-member.tsx
+++ src/pages/daos/add-new-member.tsx
@@ -3,12 +3,14 @@ import { NextPage } from "next";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { ParsedUrlQuery } from "querystring";
+import { useSigner } from "wagmi";
+import toast from "react-hot-toast";
 
 import Layout from "components/Layout/Layout";
 import { SubmitButton, InputText, BlockchainSelector, InputTextArea } from "components/Form";
 import BackButton from "components/Button/backButton";
 import NFTCardMockup from "components/Cards/NFTCard";
-
+import { validateForm } from "utils/validate";
 import { IAddNewMember } from "types/forms";
 import {
     handleTextChangeAddNewMember,
@@ -25,24 +27,42 @@ const AddNewMember: NextPage = () => {
     const [formData, setFormData] = useState<IAddNewMember>({
         walletAddress: "",
         nftID: "",
-        blockchainSelected: "",
+        blockchainSelected: "Ethereum",
         note: "",
         daoName: "",
     });
 
     const router = useRouter();
+    const { data: signer_data } = useSigner();
+
     useEffect(() => {
         const query = router.query as QueryUrlParams;
         handleChangeBasicNewMember(query.nftAddress, setFormData, "nftID");
         handleChangeBasicNewMember(query.daoName, setFormData, "daoName");
         console.log(`governorAddress from query: ${query.daoName}`);
     }, []);
+
+    async function sendSignatureRequest(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        // TODO: Need to create Signature Request
+        // https://wagmi.sh/examples/sign-in-with-ethereum
+        if (!signer_data) {
+            toast.error("Please connect wallet");
+            return;
+        }
+        if (!validateForm(formData, ["note"])) {
+            return;
+        }
+    }
     return (
         <div>
             <Layout className="layout-base">
                 <section className="relative w-full">
                     <BackButton />
-                    <form className="mx-auto flex max-w-4xl flex-col gap-4">
+                    <form
+                        className="mx-auto flex max-w-4xl flex-col gap-4"
+                        onSubmit={sendSignatureRequest}
+                    >
                         <h1 className="text-highlighter">Become a member of</h1>
                         <h1 className="text-highlighter mt-0 text-purple">{formData.daoName}</h1>
                         <InputText
