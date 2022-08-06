3cbf4154b19d1c5070e482fa64f747d9100fafb2
Vakurin <vakurin.maxim@gmail.com>
2022-08-07 02:02:46 +0300

    fix datetime

--- src/utils/basic.ts
+++ src/utils/basic.ts
@@ -7,5 +7,8 @@ export const arrayIsEmpty = <T extends string>(array: T[]) => array.length === 0
 
 export const timestampToDate = (timestamp: number) => {
     const d = new Date(timestamp * 1000);
-    return d.getHours() + ":" + d.getMinutes() + ", " + d.toDateString();
+    // Mon Aug 08 2022 01:37:19 GMT+0300 (Eastern European Summer Time)
+    const datatext = d.toTimeString()
+    // ['01:37:19', 'GMT+0300', '(Eastern', 'European', 'Summer', 'Time)']
+    return datatext.split(' ')[0] + ", " + d.toDateString();
 };
