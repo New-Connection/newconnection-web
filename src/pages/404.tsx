b81b0593768df53fa5a6f09dd8f7425e68427d95
Vakurin <vakurin.maxim@gmail.com>
2022-08-15 02:52:53 +0300

    fix 404 page mobile design

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -9,7 +9,7 @@ const Page404: NextPage = () => {
     return (
         <div>
             <Layout className="layout-base pb-0 pt-0 h-32">
-                <section className="relative w-1/2">
+                <section className="w-1/2">
                     <div className="flex items-center justify-center pt-36">
                         <h1 className="text-highlighter">
                             {"404 - We couldn't find that page :("}
@@ -17,7 +17,7 @@ const Page404: NextPage = () => {
                     </div>
                     <div className="flex items-center justify-center">
                         <Link href="/">
-                            <Button className="mt-10 w-1/3 h-14 secondary-button">
+                            <Button className="mt-10 lg:w-1/3 w-full h-14 secondary-button">
                                 Back to Home
                             </Button>
                         </Link>
