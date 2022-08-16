dea579cd4a376e95f86af3d34a3444fa2d2163fb
Vakurin <vakurin.maxim@gmail.com>
2022-08-16 22:11:47 +0300

    added google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,19 +7,39 @@ import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import { WalletConfig } from "../components/Web3";
 import { MoralisProvider } from "react-moralis";
+import Script from "next/script";
 
 function App({ Component, pageProps }: AppProps) {
     const [queryClient] = React.useState(() => new QueryClient());
+
     return (
-        <MoralisProvider appId={moralisAppId!} serverUrl={moralisServerUrl!}>
-            <ThemeProvider defaultTheme="system" attribute="class">
-                <WagmiConfig client={WalletConfig}>
-                    <QueryClientProvider client={queryClient}>
-                        <Component {...pageProps} />
-                    </QueryClientProvider>
-                </WagmiConfig>
-            </ThemeProvider>
-        </MoralisProvider>
+        <>
+            <Script
+                strategy="lazyOnload"
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+            />
+
+            <Script strategy="lazyOnload">
+                {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+            </Script>
+
+            <MoralisProvider appId={moralisAppId!} serverUrl={moralisServerUrl!}>
+                <ThemeProvider defaultTheme="system" attribute="class">
+                    <WagmiConfig client={WalletConfig}>
+                        <QueryClientProvider client={queryClient}>
+                            <Component {...pageProps} />
+                        </QueryClientProvider>
+                    </WagmiConfig>
+                </ThemeProvider>
+            </MoralisProvider>
+        </>
     );
 }
 
