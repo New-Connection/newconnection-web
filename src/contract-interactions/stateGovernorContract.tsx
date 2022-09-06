6d787bbbde0c74d0d6677bf14d822337114ee3b5
Vakurin <vakurin.maxim@gmail.com>
2022-09-06 14:40:50 +0300

    update proposal

--- src/contract-interactions/stateGovernorContract.tsx
+++ src/contract-interactions/stateGovernorContract.tsx
@@ -1,16 +1,23 @@
 import { ethers, Signer } from "ethers";
-import { GOVERNOR_ABI } from "../abis";
+import { GOVERNOR_ABI, GOVERNANCE_NFT_ABI } from "../abis";
 
-export async function createProposal(contractAddress: string, signer: Signer, proposalDescription) {
+export async function createProposal(
+    contractAddress: string,
+    signer: Signer,
+    proposalDescription: string,
+    tokenAddress: string
+) {
     const governor = new ethers.Contract(contractAddress, GOVERNOR_ABI, signer);
     const encodedFunctionCall = governor.interface.encodeFunctionData("incrementExecutedProposals");
-
+    const nftToken = new ethers.Contract(tokenAddress, GOVERNANCE_NFT_ABI, signer);
     const proposeTx = await governor.propose(
         [contractAddress],
         [0],
         [encodedFunctionCall],
-        proposalDescription
+        proposalDescription,
+        nftToken.address
     );
+
     const proposeReceipt = await proposeTx.wait();
 
     return proposeReceipt.events![0].args!.proposalId.toString();
