a170dfb1279a730299a08d901aed2d707f39c585
Vakurin <vakurin.maxim@gmail.com>
2022-09-25 16:45:12 +0300

    for production

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -12,8 +12,8 @@ export const BLOCKS_IN_DAY = 6545;
 export const SECONDS_IN_BLOCK = 13.2;
 
 //Moralis
-export const moralisAppId = process.env.NEXT_PUBLIC_MORALIS_APP_ID_M;
-export const moralisServerUrl = process.env.NEXT_PUBLIC_MORALIS_SERVER_ID_URL_M;
+export const moralisAppId = process.env.NEXT_PUBLIC_MORALIS_APP_ID;
+export const moralisServerUrl = process.env.NEXT_PUBLIC_MORALIS_SERVER_ID_URL;
 
 // export const moralisAppId = process..env.NEXT_PUBLIC_MORALIS_APP_ID_DEV_M;
 // export const moralisServerUrl = process..env.NEXT_PUBLIC_MORALIS_SERVER_ID_URL_DEV_M;
