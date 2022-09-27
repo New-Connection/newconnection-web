1e6f6733c4da24c58661024237962259f04d5e6d
Vakurin <vakurin.maxim@gmail.com>
2022-09-27 15:11:23 +0300

    change images structure

---
+++ src/assets/index.tsx
@@ -0,0 +1,43 @@
+// social networks
+import discord from "/src/assets/social/Discord-Icon.png";
+import twitter from "/src/assets/social/Twitter-Icon.png";
+import website from "/src/assets/social/globe.png";
+// wallets
+import metamask from "/src/assets/wallets/mm.png";
+import walletConnect from "/src/assets/wallets/wc.png";
+
+// mockups
+import daoCoverMock from "/src/assets/mockups/basic-dao-cover.png";
+import daoLogoMock from "/src/assets/mockups/basic-dao-logo.png";
+import daoNFTMock from "/src/assets/mockups/basic-nft-image.png";
+
+// icons
+import backIcon from "/src/assets/icons/back.png";
+import imageIcon from "/src/assets/icons/image.png";
+import smartContractIcon from "/src/assets/icons/smartContract.png";
+import viewAllIcon from "/src/assets/icons/viewAll.png";
+
+import defaultToken from "/src/assets/chains/emptyToken.webp";
+
+const ASSETS = {
+    // social networks
+    discord: discord,
+    twitter: twitter,
+    website: website,
+    // wallets
+    metamask: metamask,
+    walletConnect: walletConnect,
+    // mockups
+    daoCoverMock: daoCoverMock,
+    daoLogoMock: daoLogoMock,
+    daoNFTMock: daoNFTMock,
+    // icons
+    backIcon: backIcon,
+    imageIcon: imageIcon,
+    smartContractIcon: smartContractIcon,
+    viewAllIcon: viewAllIcon,
+    // tokens
+    defaultToken: defaultToken,
+};
+
+export default ASSETS;
