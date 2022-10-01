5ba64c097380a8108932e77fa02eae13b002a7f8
retw <ernestzulpukarov@gmail.com>
2022-10-01 21:35:05 +0600

    refactor all proposals page

--- src/pages/daos/[url]/proposals/index.tsx
+++ src/pages/daos/[url]/proposals/index.tsx
@@ -1,33 +1,20 @@
-import { NextPage, NextPageContext } from "next";
+import { NextPage } from "next";
 import Link from "next/link";
 import Layout from "components/Layout";
 import { useMoralisQuery } from "react-moralis";
-import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { IDAOPageForm, IProposalPageForm } from "types/forms";
-import {
-    isProposalActive,
-    proposalAgainstVotes,
-    proposalDeadline,
-    proposalForVotes,
-} from "contract-interactions";
 import { ProposalCard } from "components/Cards/";
 import * as React from "react";
 import { useRouter } from "next/router";
 import { BackButton } from "components/Button/";
 import { IProposalsQuery } from "types/queryInterfaces";
 import { MockupLoadingProposals } from "components/Mockup/Loading";
-
-const useIsomorphicLayoutEffect = typeof window !== "undefined" ? useLayoutEffect : useEffect;
-
-export const getServerSideProps = async (context: NextPageContext) => {
-    const { query } = context;
-    return { props: { query } };
-};
+import { fetchProposal } from "network";
 
 const ProposalsPage: NextPage = () => {
     const [DAO, setDAO] = useState<IDAOPageForm>();
     const [proposals, setProposals] = useState<IProposalPageForm[]>();
-    const firstUpdate = useRef(true);
     const router = useRouter();
 
     const { fetch: ProposalQuery } = useMoralisQuery(
@@ -37,67 +24,39 @@ const ProposalsPage: NextPage = () => {
             query.equalTo("chainId", DAO?.chainId),
         [DAO],
         {
-            autoFetch: false,
+            autoFetch: false
         }
     );
 
-    const fetchProposal = async () => {
-        await ProposalQuery({
-            onSuccess: async (results) => {
-                const proposals: IProposalPageForm[] = await Promise.all(
-                    results.map(async (proposalMoralis) => {
-                        const governorAddress = proposalMoralis.get("governorAddress");
-                        const chainId = proposalMoralis.get("chainId");
-                        const proposalId = proposalMoralis.get("proposalId");
-                        const proposal: IProposalPageForm = {
-                            name: proposalMoralis.get("name"),
-                            governorAddress: governorAddress,
-                            chainId: chainId,
-                            proposalId: proposalId,
-                            description: proposalMoralis.get("description"),
-                            shortDescription: proposalMoralis.get("shortDescription"),
-                            isActive: await isProposalActive(governorAddress, chainId, proposalId),
-                            forVotes: await proposalForVotes(governorAddress, chainId, proposalId),
-                            againstVotes: await proposalAgainstVotes(
-                                governorAddress,
-                                chainId,
-                                proposalId
-                            ),
-                            deadline: await proposalDeadline(governorAddress, chainId, proposalId),
-                            options: [],
-                            blockchain: [],
-                        };
-                        return proposal;
-                    })
-                );
-                setProposals(() => proposals);
-            },
-            onError: (error) => {
-                console.log("Error fetching db query" + error);
-            },
-        });
-    };
+    useEffect(() => {
+        const fetchQuery = () => {
+            const query = router.query as IProposalsQuery;
+
+            const newDao = {} as IDAOPageForm;
+            newDao.governorAddress = query.governorAddress;
+            newDao.name = query.name;
+            newDao.chainId = +query.chainId;
+
+            console.log(newDao);
+            setDAO(() => newDao);
+        };
+
+        fetchQuery();
+    }, [router]);
 
     useEffect(() => {
-        const query = router.query as IProposalsQuery;
-        console.log("query:" + router.query.daoAddress);
+        if (DAO) {
+            const loadingProposals = async () => {
+                const proposals = await fetchProposal(ProposalQuery);
+                if (proposals) {
+                    setProposals(() => proposals);
+                }
+            };
 
-        const newDao = {} as IDAOPageForm;
-        // newDao.url = query.address;
-        newDao.governorAddress = query.governorAddress;
-        newDao.name = query.name;
-        newDao.chainId = +query.chainId;
-
-        console.log(newDao);
-        setDAO(() => newDao);
-    }, []);
-
-    useIsomorphicLayoutEffect(() => {
-        if (DAO && firstUpdate.current) {
-            fetchProposal();
-            firstUpdate.current = false;
+            console.log("dao load");
+            loadingProposals().catch(console.error);
         }
-    });
+    }, [DAO]);
 
     return DAO ? (
         <div>
@@ -105,20 +64,13 @@ const ProposalsPage: NextPage = () => {
                 <section className="app-section flex h-full flex-1 flex-col gap-[50px]">
                     <BackButton />
                     <div className={"flex justify-between items-center"}>
-                        <h1 className={"text-highlighter cap"}>{DAO.name} Proposals</h1>
+                        <h1 className={"text-highlighter cap capitalize"}>{DAO.name} Proposals</h1>
                     </div>
 
                     {proposals && proposals.length !== 0 ? (
                         <ul>
                             {proposals.map((proposal) => {
                                 const proposalId = proposal.proposalId;
-                                const name = proposal.name;
-                                const description = proposal.description;
-                                const shortDescription = proposal.shortDescription;
-                                const isActive = proposal.isActive;
-                                const forVotes = proposal.forVotes;
-                                const againstVotes = proposal.againstVotes;
-                                const deadline = proposal.deadline;
                                 return (
                                     <Link href={`proposals/${proposalId}`} key={proposalId}>
                                         <li
@@ -126,15 +78,15 @@ const ProposalsPage: NextPage = () => {
                                             className="border-b-2 border-gray cursor-pointer active:bg-gray"
                                         >
                                             <ProposalCard
-                                                title={name}
-                                                description={description}
-                                                shortDescription={shortDescription}
+                                                title={proposal.name}
+                                                description={proposal.description}
+                                                shortDescription={proposal.shortDescription}
                                                 daoName={DAO?.name}
                                                 chainId={DAO?.chainId}
-                                                isActive={isActive}
-                                                forVotes={forVotes}
-                                                againstVotes={againstVotes}
-                                                deadline={deadline}
+                                                isActive={proposal.isActive}
+                                                forVotes={proposal.forVotes}
+                                                againstVotes={proposal.againstVotes}
+                                                deadline={proposal.deadline}
                                             />
                                         </li>
                                     </Link>
