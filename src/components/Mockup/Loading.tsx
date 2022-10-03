b8c63771605cff2a0a7589c8586197df5651fc1c
Vakurin <vakurin.maxim@gmail.com>
2022-10-03 16:20:50 +0300

    NFT Mockup

--- src/components/Mockup/Loading.tsx
+++ src/components/Mockup/Loading.tsx
@@ -51,17 +51,32 @@ export const MockupLoadingProposals = ({ chain }: { chain: number | string }) =>
 
 export const MockupLoadingNFT = ({ chain }: { chain: number | string }) => {
     return (
-        <div className="nft-card animate-pulse ">
-            {/* //Wrap to div for center elements */}
-            <div className="mt-4 mx-4 h-36 w-full-8 bg-gray2 rounded"></div>
-            <div className="p-4 gap-y-6">
-                <div className="flex justify-between">
-                    <div className="h-2.5 w-20 bg-gray2 rounded"></div>
-                    <div className="h-2.5 w-8 bg-gray2 rounded"></div>
+        <div className="flex flex-col overflow-hidden w-72 rounded-lg border border-gray3">
+            <div className="flex-shrink-0">
+                <div className="animate-pulse h-72 w-full object-cover bg-gray2" />
+            </div>
+            <div className="flex flex-1 flex-col justify-between bg-white px-6 pt-2 pb-6">
+                <div className="flex-1">
+                    <div className="mt-1 flex justify-between">
+                        <p className="text-base font-medium animation-pulse bg-gray2 text-gray2 mt-1">
+                            Nft membership
+                        </p>
+                        {/* TODO: Pass variable how many minted NFT and how many left */}
+                        <p className="text-base font-medium ml-4 mt-2 animate-pulse bg-gray2 text-gray2">
+                            499/500
+                        </p>
+                    </div>
                 </div>
-                <div className="flex pt-4 justify-between">
-                    <div className="h-2.5 w-14 bg-gray2 rounded"></div>
-                    <BlockchainIcon chain={chain} />
+                <div className="mt-[0.75rem] flex items-center justify-between">
+                    <div className="flex-shrink-0">
+                        <p className="text-sm font-normal animate-pulse bg-gray2 text-gray2">
+                            0x0000...0000
+                        </p>
+                    </div>
+                    <div className="ml-3 flex gap-2">
+                        <p className="text-sm font-normal animate-pulse bg-gray2 text-gray2">000</p>
+                        <BlockchainIcon chain={chain} />
+                    </div>
                 </div>
             </div>
         </div>
