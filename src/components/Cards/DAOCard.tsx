15281a5cd4411d008aaa165fe4642c5d161adc71
Vakurin <vakurin.maxim@gmail.com>
2022-10-03 11:32:37 +0300

    fix DAOCard Design

--- src/components/Cards/DAOCard.tsx
+++ src/components/Cards/DAOCard.tsx
@@ -9,23 +9,27 @@ export const DAOCard = ({ name, description, profileImage, url, isActive, propos
         <Link href={`/daos/${url}`}>
             <div
                 className={
-                    "flex justify-between w-full lg:h-40 h-50 p-3 border-b-2 border-gray cursor-pointer active:bg-gray"
+                    "flex justify-between w-full h-50 p-3 border-b border-gray cursor-pointer active:bg-gray"
                 }
             >
-                <div className="flex gap-10 w-10/12">
+                <div className="flex gap-6 w-10/12 pt-3 pb-3">
                     <div className="w-28 h-28">
-                        <Image
-                            width={"150"}
-                            height={"150"}
-                            layout={"responsive"}
-                            priority={true}
-                            src={!isIpfsAddress(profileImage) ? profileImage : ASSETS.daoLogoMock}
-                            className="rounded-2xl"
-                        />
+                        {!isIpfsAddress(profileImage) ? (
+                            <Image
+                                width={"115"}
+                                height={"115"}
+                                layout={"fixed"}
+                                priority={true}
+                                src={profileImage || ASSETS.daoLogoMock}
+                                className="rounded-2xl"
+                            />
+                        ) : (
+                            <div className="w-28 h-28 bg-gray2 rounded-2xl animate-pulse"></div>
+                        )}
                     </div>
                     <div className="w-5/6 grid grid-cols-1 content-between">
                         <div className="w-full">
-                            <p className="lg:text-lg text-base uppercase font-semibold cursor-pointer">
+                            <p className="lg:text-lg text-base uppercase font-medium cursor-pointer">
                                 {name}
                             </p>
                             <div className="text-gray-500 line-clamp-2">{description}</div>
@@ -41,16 +45,25 @@ export const DAOCard = ({ name, description, profileImage, url, isActive, propos
                     </div>
                 </div>
 
-                <div className="flex flex-col lg:w-32 w-24 text-center text-xs">
+                <div className="flex flex-col lg:w-32 w-28 text-center text-xs pt-3 pb-3">
                     {isActive ? (
                         <span className="badge-active">Active voting now</span>
                     ) : (
-                        <span className="badge-active text-black">No active voting</span>
+                        <span className="badge-active text-black ">No active voting</span>
                     )}
                     <div className={"flex flex-col gap-3 mt-4"}>
                         <div className={"flex justify-between"}>
                             <p className={"text-gray2"}>Proposals:</p>
-                            <p>{proposals || 0}</p>
+                            <p>
+                                {proposals ? (
+                                    proposals || 0
+                                ) : (
+                                    //MOCK UP FOR LOADING
+                                    <div className="bg-gray2 text-gray2 animate-pulse rounded-md">
+                                        000
+                                    </div>
+                                )}
+                            </p>
                         </div>
                         {/* <div className={"flex justify-between"}>
                             <p className={"text-gray2"}>Votes:</p>
